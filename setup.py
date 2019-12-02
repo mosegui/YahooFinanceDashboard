c4c76e32ac3e8d03913e64bff9bcf0ab2618a7ea
Daniel Moseguí González <mosegui@MUC.FOS4X.DE>
2019-12-02 22:16:15 +0100

    debugging new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="YahooFinanceDashboard",
-    version="1.0.7",
+    version="1.0.8",
     author="Daniel Moseguí González",
     author_email="d.mosegui@gmail.com",
     description="API for accessing, synchronizing, managing locally and plotting Yahoo financial data",
